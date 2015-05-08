1e03e48ff8a7549d8f1ef52817091abec285683f
Colby Skeggs <skeggsc@devel-libca-skeggsc>
2015-05-08 14:58:42 -0700

    Some initial on linux-x86.

---
+++ linux-x86/entry.c
@@ -0,0 +1,48 @@
+// 32-bit x86 only
+
+#include <stdint.h>
+
+#define AT_NULL         0               /* End of vector */
+#define AT_IGNORE       1               /* Entry should be ignored */
+#define AT_EXECFD       2               /* File descriptor of program */
+#define AT_PHDR         3               /* Program headers for program */
+#define AT_PHENT        4               /* Size of program header entry */
+#define AT_PHNUM        5               /* Number of program headers */
+#define AT_PAGESZ       6               /* System page size */
+#define AT_BASE         7               /* Base address of interpreter */
+#define AT_FLAGS        8               /* Flags */
+#define AT_ENTRY        9               /* Entry point of program */
+#define AT_NOTELF       10              /* Program is not ELF */
+#define AT_UID          11              /* Real uid */
+#define AT_EUID         12              /* Effective uid */
+#define AT_GID          13              /* Real gid */
+#define AT_EGID         14              /* Effective gid */
+#define AT_CLKTCK       17              /* Frequency of times() */
+/* Pointer to the global system page used for system calls and other nice things.  */
+#define AT_SYSINFO      32
+#define AT_SYSINFO_EHDR 33
+
+extern uint32_t *_lsc_init_ptr;
+static void *_lsc_vdso_ptr = NULL;
+static uint32_t _lsc_argc;
+static char **_lsc_argv;
+static uint32_t *_lsc_auxv;
+static char *should_be_null;
+
+// must not return
+void _lsc_start() {
+	_lsc_argc = *_lsc_init_ptr;
+	_lsc_argv = _lsc_init_ptr + 1;
+	should_be_null = _lsc_argv[argc];
+	_lsc_env = _lsc_init_ptr + 2 + argc;
+	uint32_t *cur = _lsc_env;
+	while (*(cur++) != NULL);
+	_lsc_auxv = cur;
+	while (*(cur) != AT_NULL) {
+		if (*(cur++) == AT_SYSINFO) {
+			_lsc_vdso_ptr = *(cur++);
+			break;
+		}
+	}
+	_lsc_syscall1(_lsc_vdso_ptr, 1, 37);
+}
--- linux-x86/entry.s
+++ linux-x86/entry.s
@@ -1,30 +1,36 @@
 .text
 .globl _start
-.extern _lsc_main
+.extern _lsc_start
 _start:
 	xorl %ebp, %ebp
 	movl _lsc_init_ptr, %eax
 	movl %esp, (%eax)
 	andl $0xFFFFFFF0, %esp
-	jmp _lsc_main
-.globl _lsc_syscall
-_lsc_syscall:
-	movl (%esp), %eax
-	movl 4(%esp), $ebx
-	movl 8(%esp), $ecx
-	movl 12(%esp), $edx
-	movl 16(%esp), $esi
-	movl 20(%esp), $edi
-	pushl %ebp
-	movl _lsc_syscall_ptr, %ebp
-	movl (%ebp), %ebp
+	jmp _lsc_start
+.globl _lsc_syscall5
+.globl _lsc_syscall4
+.globl _lsc_syscall3
+.globl _lsc_syscall2
+.globl _lsc_syscall1
+.globl _lsc_syscall0
+_lsc_syscall5:
+	movl 28(%esp), $edi
+_lsc_syscall4:
+	movl 24(%esp), $esi
+_lsc_syscall3:
+	movl 20(%esp), $edx
+_lsc_syscall2:
+	movl 16(%esp), $ecx
+_lsc_syscall1:
+	movl 12(%esp), $ebx
+_lsc_syscall0:
+	movl 8(%esp), %eax
+	xchgl 4(%esp), %ebp
 	call *%ebp
-	popl %ebp
+	movl 4(%esp), %ebp
 	; pass %eax out
 	ret
 
 .data
 .globl _lsc_init_ptr
 _lsc_init_ptr: dd 0
-.globl _lsc_syscall_ptr
-_lsc_syscall_ptr: dd 0
